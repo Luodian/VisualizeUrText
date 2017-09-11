a1930943d2c7e353ce29ad29cf87512dd7ba2731
PracNeyman <1799022371@qq.com>
2017-09-11 23:05:52 +0800

    整合

---
+++ src/sample/DirectedGraphInterface.java
@@ -0,0 +1,19 @@
+package sample;
+
+public interface DirectedGraphInterface<T>
+{
+    /*
+        Task:Add new vertice with label
+        @param:label
+     */
+
+    public void addVertex(T vertexLabel);
+    public void addEdge(T begin,T end,double edgeWeight);
+    public void addEdge(T begin,T end);
+    public boolean hasEdge(T begin,T end);
+    public boolean isEmpty();
+    public int getNumberOfVertices();
+    public int getNumberOfEdges();
+    public void clear();
+    public void f(T vertexLabel);
+}
